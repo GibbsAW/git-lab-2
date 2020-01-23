b52a490ce6f207546e037b78dca1667266a4a2f2
Alexander Gibbs <ag135217@ohio.edu>
2020-01-22 19:07:58 -0500

    Created a simple text program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Alexander Gibbs
+ *        Date: 1-22-2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git...\n";
+    return 0;
+}// main
